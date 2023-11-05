ee781b91c81a8028589ccaaf0d8010487918f896
Dilawar Amin <79779873+dilawaramin@users.noreply.github.com>
2023-11-05 16:17:21 -0500

    Create city.py

---
+++ Assignment 2 Code/city.py
@@ -0,0 +1,19 @@
+##### generate the city layout to be used by Q-learning #####
+
+# Use NetworkX library, represent city as a graph with nodes and edges
+# NOTE: Use "pip install networkx" beforehand
+import networkx as nx 
+
+
+def generate_city(grid_size, num_intersections, num_streets):
+    """
+    Function that creates cities
+        Parameters:
+            grid_size: desired overall size of city
+            num_intersections: desired # of intersections
+            num_streets: desired number of streets
+        Returns:
+            G: Graph, representing city as a network of nodes and edges
+    """
+    
+    return
