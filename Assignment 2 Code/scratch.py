26d371eb223052487e9074a918cd572bab7d981d
Dilawar Amin <79779873+dilawaramin@users.noreply.github.com>
2023-11-07 20:47:41 -0500

    more touching up

--- Assignment 2 Code/scratch.py
+++ Assignment 2 Code/scratch.py
@@ -1,9 +1,1 @@
-import os
 
-try:
-  os.system('pause')  #windows, doesn't require enter
-except whatever_it_is:
-  os.system('read -p "Press any key to continue"') #linux
-  
-print("Function complete")
-g
